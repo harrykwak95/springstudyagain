4cf83b43eb5b975fe6f74a5c2010f52bbeeb5413
gwakminwoo <gwakminwoo@twave.co.kr>
2023-07-19 19:52:38 +0900

    feat : DataLoader 추가, 외 간단한 리팩토링(주석 제거, 삼항 연산자 관련 변경)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import org.springframework.boot.SpringApplication;
@@ -7,8 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +24,25 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+@Component
+class DataLoader {
+	private final CoffeeRepository coffeeRepository;
+
+	public DataLoader(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
+	}
+
+	@PostConstruct
+	private void loadData() {
+		coffeeRepository.saveAll(List.of(
+				new Coffee("Cafe 1"),
+				new Coffee("Cafe 2"),
+				new Coffee("Cafe 3"),
+				new Coffee("Cafe 4")
+		));
+	}
 }
 
 interface CoffeeRepository extends CrudRepository<Coffee, String> {}
@@ -71,87 +93,39 @@ class RestApiDemoController {
 
 	public RestApiDemoController(CoffeeRepository coffeeRepository) {
 		this.coffeeRepository = coffeeRepository;
-
-		this.coffeeRepository.saveAll(List.of(
+/*		this.coffeeRepository.saveAll(List.of(
 				new Coffee("Cafe Cereza"),
 				new Coffee("Cafe Ganador"),
 				new Coffee("Cafe Lara"),
 				new Coffee("Cafe Tres")
-		));
+		));*/
 	}
 
-/*	private List<Coffee> coffees = new ArrayList<>();
-
-	public RestApiDemoController() {
-		coffees.addAll(List.of(
-				new Coffee("Cafe Cereza"),
-				new Coffee("Cafe Ganador"),
-				new Coffee("Cafe Lara"),
-				new Coffee("Cafe Tres")
-		));
-	}*/
-
-/*	@RequestMapping(value = "/coffees", method = RequestMethod.GET)
-	Iterable<Coffee> getCoffees() {
-		return coffees;
-	}*/
-
-	/*@GetMapping("/coffees")*/
 	@GetMapping
 	Iterable<Coffee> getCoffees() {
 		return coffeeRepository.findAll();
 	}
-	/*Iterable<Coffee> getCoffees() {
-		return coffees;
-	}*/
 
-	/*@GetMapping("/coffees/{id}")*/
 	@GetMapping("/{id}")
 	Optional<Coffee> getCoffeeById(@PathVariable String id) {
 		return coffeeRepository.findById(id);
 	}
-/*	Optional<Coffee> getCoffeeById(@PathVariable String id) {
-		for (Coffee c : coffees) {
-			if (c.getId().equals(id)) {
-				return Optional.of(c);
-			}
-		}
-
-		return Optional.empty();
-	}*/
 
 	@PostMapping
 	Coffee postCoffee(@RequestBody Coffee coffee) {
 		return coffeeRepository.save(coffee);
 	}
-/*	Coffee postCoffee(@RequestBody Coffee coffee) {
-		coffees.add(coffee);
-		return coffee;
-	}*/
 
 	@PutMapping("/{id}")
 	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-/*		int coffeeIndex = -1;
 
-		for (Coffee c : coffees) {
-			if (c.getId().equals(id)) {
-				coffeeIndex = coffees.indexOf(c);
-				coffees.set(coffeeIndex, coffee);
-			}
-		}
-
-		return (coffeeIndex == -1) ?
-				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
-				new ResponseEntity<>(coffee, HttpStatus.OK);*/
-
-		return (!coffeeRepository.existsById(id))
-				? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED)
-				: new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK);
+		return (coffeeRepository.existsById(id))
+				? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK)
+				: new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
 	}
 
 	@DeleteMapping("/{id}")
 	void deleteCoffee(@PathVariable String id) {
 		coffeeRepository.deleteById(id);
-		/*coffees.removeIf(c -> c.getId().equals(id));*/
 	}
 }
